ea5108718d1d4cd2e4fdb695043b509dc5e5df26
Cristian Romanello <krystian1284@gmail.com>
2020-03-09 19:36:58 +0100

    Firebase Notifications

---
+++ backend/server.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+
+# https://gist.github.com/bradmontgomery/2219997
+
+"""
+Very simple HTTP server in python (Updated for Python 3.7)
+
+Usage:
+
+    ./dummy-web-server.py -h
+    ./dummy-web-server.py -l localhost -p 8000
+
+Send a GET request:
+
+    curl http://localhost:8000
+
+Send a HEAD request:
+
+    curl -I http://localhost:8000
+
+Send a POST request:
+
+    curl -d "foo=bar&bin=baz" http://localhost:8000
+
+"""
+import argparse
+import cgi
+from http.server import HTTPServer, BaseHTTPRequestHandler
+from urllib.parse import urlparse
+
+
+class S(BaseHTTPRequestHandler):
+
+    def format_response(self, response):
+        return str(response).encode("utf-8")
+
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+
+    def do_GET(self):
+        self._set_headers()
+        response = "Ciao scemo!"
+        self.wfile.write(self.format_response(response))
+
+    def do_HEAD(self):
+        self._set_headers()
+
+    def do_POST(self):
+        form = cgi.FieldStorage(
+            fp=self.rfile,
+            headers=self.headers,
+            environ={'REQUEST_METHOD': 'POST'}
+        )
+        for key in form.keys():
+            print(key + " = " + form.getvalue(key))
+
+        self._set_headers()
+        self.wfile.write(self.format_response(200))
+
+
+def run(server_class=HTTPServer, handler_class=S, addr="localhost", port=8000):
+    server_address = (addr, port)
+    httpd = server_class(server_address, handler_class)
+
+    print(f"Starting httpd server on {addr}:{port}")
+    httpd.serve_forever()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Run a simple HTTP server")
+    parser.add_argument(
+        "-l",
+        "--listen",
+        default="localhost",
+        help="Specify the IP address on which the server listens",
+    )
+    parser.add_argument(
+        "-p",
+        "--port",
+        type=int,
+        default=8000,
+        help="Specify the port on which the server listens",
+    )
+    args = parser.parse_args()
+    run(addr=args.listen, port=args.port)
