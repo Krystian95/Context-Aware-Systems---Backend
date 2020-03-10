766cb59e58b58e4d1ae593271a57c6d5933e6b8d
Cristian Romanello <krystian1284@gmail.com>
2020-03-10 16:48:03 +0100

    Actions on Server

--- backend/Server.py
+++ backend/Server.py
@@ -1,38 +1,21 @@
-#!/usr/bin/env python
 
 # https://gist.github.com/bradmontgomery/2219997
 
-"""
-Very simple HTTP server in python (Updated for Python 3.7)
-
-Usage:
-
-    ./dummy-web-server.py -h
-    ./dummy-web-server.py -l localhost -p 8000
-
-Send a GET request:
-
-    curl http://localhost:8000
-
-Send a HEAD request:
-
-    curl -I http://localhost:8000
-
-Send a POST request:
-
-    curl -d "foo=bar&bin=baz" http://localhost:8000
-
-"""
 import argparse
 import cgi
 from http.server import HTTPServer, BaseHTTPRequestHandler
-from urllib.parse import urlparse
 
 
-class S(BaseHTTPRequestHandler):
+def print_post_params(form):
+    for key in form.keys():
+        print(key + " = " + form.getvalue(key))
 
-    def format_response(self, response):
-        return str(response).encode("utf-8")
+
+def format_response(response):
+    return str(response).encode("utf-8")
+
+
+class Server(BaseHTTPRequestHandler):
 
     def _set_headers(self):
         self.send_response(200)
@@ -41,8 +24,7 @@ class S(BaseHTTPRequestHandler):
 
     def do_GET(self):
         self._set_headers()
-        response = "Ciao scemo!"
-        self.wfile.write(self.format_response(response))
+        self.wfile.write(format_response(200))
 
     def do_HEAD(self):
         self._set_headers()
@@ -53,14 +35,28 @@ class S(BaseHTTPRequestHandler):
             headers=self.headers,
             environ={'REQUEST_METHOD': 'POST'}
         )
-        for key in form.keys():
-            print(key + " = " + form.getvalue(key))
+
+        action = form.getvalue("action")
+        params = form
+
+        if action == "register":
+            self.action_register(params)
+        elif action == "communicate-position":
+            self.action_communicate_position(params)
+        else:
+            print("Requested operation not recognized")
 
         self._set_headers()
-        self.wfile.write(self.format_response(200))
+        self.wfile.write(format_response(200))
 
+    def action_register(self, params):
+        print_post_params(params)
 
-def run(server_class=HTTPServer, handler_class=S, addr="localhost", port=8000):
+    def action_communicate_position(self, params):
+        print_post_params(params)
+
+        
+def run(server_class=HTTPServer, handler_class=Server, addr="localhost", port=8000):
     server_address = (addr, port)
     httpd = server_class(server_address, handler_class)
 
